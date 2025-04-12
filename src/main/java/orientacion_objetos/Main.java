803a636c16b4698b5e1aebcd7f4307219fb8fc3b
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2025-04-12 19:00:39 -0400

    Update Main.java

--- src/main/java/orientacion_objetos/Main.java
+++ src/main/java/orientacion_objetos/Main.java
@@ -1,7 +1,7 @@
 package orientacion_objetos;
 import javax.swing.SwingUtilities;
 import orientacion_objetos.vista.VistaCalculo;
-
+//Rodolfo Parada Gónzalez
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new VistaCalculo());
